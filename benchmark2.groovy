2c5ec8f9852229357927501e7fcc965959a28d9f
Aranir <rogerkueng@gmail.com>
2015-05-16 12:39:59 +0200

    simplify queery 3

--- benchmark2.groovy
+++ benchmark2.groovy
@@ -6,35 +6,39 @@ file = new File(".", 'benchmark_result.txt')
 startTime = new Date()
 
 file << "start time $startTime.time\n"
+file << "\n------------------------------------------------\n"
 file << "starting Q1: Get all nodes with energy <= 0.0f \n"
 startQ1 = new Date()
 q1 = g.V.has('energy', T.lte, 0.0f).count()
 endQ1 = new Date()
 file << "duration Q1: ${endQ1.time - startQ1.time} size: ${q1}"
 
+
+file << "\n------------------------------------------------\n"
 file << "starting Q2: Get all nodes with energy > 0.0f \n"
 startQ2 = new Date()
 q2 = g.V.has('energy', T.gt, 0.0f).count()
 endQ2 = new Date()
-file << "duration Q2: ${endQ2.time - startQ2.time} size: ${q2}"
+file << "duration Q2: ${endQ2.time - startQ2.time} size: ${q2}\n"
 
-
+file << "\n------------------------------------------------\n"
 file << "starting Q3: Get all nodes with\n" +
         "- input number_of_atoms > 3 \n" +
         "- output energy <= 0\n" +
-        "from these get the the input nodes two to 5 levels up"
+        "from these get the the input nodes two to 5 levels up\n"
 startQ3 = new Date()
-q3 = g.V.has('number_of_atoms', T.gt, 3).has('energy', T.gt, 0.0f).as('origin').in.in.as('c2').in.as('c3').in.as('c4').in.as('c5').select.count()
+q3 = g.V.has('number_of_atoms', T.gt, 3).has('energy', T.gt, 0.0f).as('origin').in.in.as('c2').in.as('c3').select.count()
 endQ3 = new Date()
-file << "duration Q3: ${endQ3.time - startQ3.time} size: ${q3}"
+file << "duration Q3: ${endQ3.time - startQ3.time} size: ${q3}\n"
+
 
 endTime = new Date()
-
+file << "\n------------------------------------------------\n"
 file << "end time ${endTime.time}"
 duration = endTime.time - startTime.time
-file << "duration total ${duration}"
-file << "duration in seconds ${duration/1000}"
-file << "duration in minutes ${duration/60}"
+file << "duration total ${duration}\n"
+file << "duration in seconds ${duration/1000}\n"
+file << "duration in minutes ${duration/60}\n"
 
 
 g.shutdown()
