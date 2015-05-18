9cf7b9957964a3abbb75c3588bee5ab23aef6edb
Aranir <rogerkueng@gmail.com>
2015-05-18 09:19:27 +0200

    Add Q4

--- benchmark2.groovy
+++ benchmark2.groovy
@@ -31,6 +31,16 @@ endQ3 = new Date()
 file << "duration Q3: ${endQ3.time - startQ3.time}ms size: ${q3}\n"
 file << "duration Q3: ${(endQ3.time - startQ3.time)/1000/60} m size: ${q3}\n"
 
+file << "\n------------------------------------------------\n"
+file << "starting Q4: Get the input node and all its attributes of the nodes with\n" +
+        "- input number_of_atoms > 3 \n" +
+        "- output energy > 0\n"
+startQ4 = new Date()
+q4 = g.V.has('number_of_atoms', T.gt, 3).has('energy', T.gt, 0.0f).in.map.dedup().count()
+endQ4 = new Date()
+file << "duration Q4: ${endQ4.time - startQ4.time}ms size: ${q4}\n"
+file << "duration Q4: ${(endQ4.time - startQ4.time)/1000/60} m size: ${q4}\n"
+
 
 
 endTime = new Date()
@@ -40,6 +50,9 @@ duration = endTime.time - startTime.time
 file << "duration total ${duration}ms\n"
 file << "duration in seconds ${duration/1000} s\n"
 file << "duration in minutes ${duration/1000/60} m\n"
+
+
+file << "===================================================\n"
 
 
 g.shutdown()
